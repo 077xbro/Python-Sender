84ab14cd4d07117f0c4c8139ae81bb470a618296
077xbro <62489365+077xbro@users.noreply.github.com>
2020-03-31 02:24:40 +0700

    Update Sendinbox.bat

--- Sendinbox.bat
+++ Sendinbox.bat
@@ -1,13 +1,1 @@
-@rem @author: ID3n3tsp3tID
-@rem @Date: 2020-01-14 07:18:07
-@rem @last Modified by: ID3n3tsp3tID
-@rem Modified time: $date
-@echo off
-set PATH=%PATH%;c:\xampp\php
-title Sendinbox Google Script CommandLine (SENBOXMAILER) 2020
-:runsendinbox
 
-php sendinbox.php
-pause
-cls
-goto runsendinbox
